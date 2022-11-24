ed24f2a7b4c6b109aa26d8e748c23ab8e4d1644a
JW15GH <96279235+JW15GH@users.noreply.github.com>
2022-11-24 11:33:10 +0000

    Add files via upload

--- CardGame.java
+++ CardGame.java
@@ -1,36 +1,41 @@
-import java.io.File;  // Import the File class
-import java.io.FileNotFoundException;  // Import this class to handle errors
-import java.util.Scanner; // Import the Scanner class to read text files
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 import java.util.ArrayList;
 
 import cardgame.Player;
 import cardgame.Card;
 import cardgame.CardDeck;
 
-/* TASKS TO DO:
-     * *DONE* player input and test validity 
-     * *DONE* pack file input and validity
-     *  player threads created
-     *  new card object created and written to pack file
-     *  cards are distributed to make list of hands and decks for each player (NOTE: Order is 1 card to each player in a round robin until 4 cards, then decks )
-     *  output file is made for players and the decks EACH
-     *  wincondition checked upon each player, if no win is made
-     *  atomic action of drawing, checking win condition and discarding for player threads
-     *  atomic action of drawing from top of deck and add from bottom of deck
-     *  wincondition is met and player that has one notifies other players so game is stopped
-     * Synchronise file writing
-     */
+/**
+ * A card game whereby a player needs 4 matching cards to win
+ * they pick up a card from a deck to their left and discard a
+ * card to their right. Each player is a thread.
+ *
+ * @author Max Ward, James White
+ * @version 1.0
+ *
+ */
 
-public class CardGame {
+ /** 
+  * A method to request the input of the number of players
+  * Verifies the input is a positive integer and rejects inputs
+  * that are negative, zero, or letters.
+  * If the number of players is valid the method requests the
+  * location of the pack file. If the pack file is found it verifies
+  * that the pack has 8n cards and contains only positive integers.
+  * The pack file is rejected if it contains letters, multiple integers
+  * or is of the incorrect length.
+  */
+  public class CardGame {
    
-    
-
     static int inputPlayers() {
         int players = 0;
     
         System.out.println("How many players are playing? ");
         Boolean test = false;
         while(test == false) {
+            // Declare scanner object
             Scanner enter = new Scanner(System.in);
             if(enter.hasNextInt()) {
                 players = enter.nextInt();
@@ -61,7 +66,7 @@ public class CardGame {
             try {
             File file = new File(pack);
             Scanner sc = new Scanner(file);
-
+            // ensuring pack only contains integers
             while(sc.hasNextLine()) {
                 String content = sc.nextLine();
                 lines++;
@@ -75,7 +80,7 @@ public class CardGame {
                     verify = 1;
                 }
             }
-
+            // ensuring the pack contains the correct number of cards
             final int constant = 8;
             int n = constant * players;
 
@@ -83,8 +88,6 @@ public class CardGame {
                 System.out.println("Pack found with correct number of cards inside");
                 sc.close();
                 test = false;
-                
-
 
             } else {
                 System.out.println("Please enter a pack with a valid number of cards in please");
@@ -96,12 +99,15 @@ public class CardGame {
             e.getStackTrace();
             }
         }
-
         return data;
     }
     
-
-
+/**
+ * Executable main method which stores the number of players
+ * inputed by the user to a variable and converts the inputed
+ * card pack from the user into an ArrayList. Begins the game
+ * and checks continuously too see if a player has won.
+ */
     public static void main(String[] args) {
         ArrayList<Player> players = new ArrayList<>();
         ArrayList<CardDeck> decks = new ArrayList<>();
@@ -114,9 +120,6 @@ public class CardGame {
             decks.add(new CardDeck(j));
         }
        distributeCards(noPlayers, players, decks, cardPack);
-       //for (int i=0;i<4;i++) {
-       //     System.out.println(players.get(0).getHand().get(i).getValue());
-       //}
         for (Player p : players) {
             p.start();
         }
@@ -131,20 +134,19 @@ public class CardGame {
             for (Player a : players) {
                 if (a.winCheck()) {
                     gameNotWon = false;
-                    //System.out.println("the winner is player"+a.getPlayerID());
-                    //System.out.println("the player had hand");
-                    //for (int i=0;i<4;i++) {
-                    //    System.out.println(a.getHandValues());
-                    //}
                 }
             }
         System.out.println("end");
-        }
-    }
-        
+                }
+            }    
     } 
 
-
+/**
+ * Method to distribute cards in a round robin fashion
+ * to all players created and once all players have 4 cards
+ * remaining cards are distributed in round robin fashion
+ * to all the decks
+ */
     public static void distributeCards(int noPlayers, ArrayList<Player> players, ArrayList<CardDeck> decks, ArrayList<Integer> cardPack){
         for (int i=1;i<noPlayers+1;i++){
             System.out.println("Giving each player a card");
